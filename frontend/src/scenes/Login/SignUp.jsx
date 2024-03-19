2a86ce5413248e573872e254e162eb9d3adcf054
vishvascipy <vishva.scipy@gmail.com>
2024-03-19 14:20:44 +0530

    MUI

--- frontend/src/scenes/Login/SignUp.jsx
+++ frontend/src/scenes/Login/SignUp.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate, useParams } from "react-router-dom";
 import axios from 'axios';
 import mainservice from '../../service/mainservice';
+import TextField from '@mui/material/TextField';
 //form handler
 
 function SignUp() {
@@ -36,42 +37,48 @@ function SignUp() {
 
     return (
         <>
-            <div className="p-4">
-                <div className="shadow p-4 mt-5 bg-body-tertiary rounded">
+            <div className="p-4 mt-5 d-flex justify-content-center">
+                <div className="shadow p-4 mt-3 bg-body-tertiary rounded">
                     <form>
-                        <h1 className="display-5 mb-4">User Registeration</h1>
+                        <h1 className="display-5 mb-4 d-flex justify-content-center">User Registeration</h1>
                         <div className="row">
-                            <div className="col">
-                                <div className="mb-3">
-                                    <label className="form-label">First Name</label>
-                                    <input type="text" className="form-control" name="firstName" onChange={onChangeHandler} />
-                                </div>
+                            {/* <div className="col"> */}
+                            <div className="mb-3 d-flex justify-content-center">
+                                <TextField id="outlined-basic" type="text" name="firstName" label="First Name" variant="outlined" onChange={onChangeHandler} />
+                                {/* <label className="form-label">First Name</label>
+                                    <input type="text" className="form-control" name="firstName" onChange={onChangeHandler} /> */}
                             </div>
+                            {/* </div> */}
 
-                            <div className="col">
-                                <div className="mb-3">
-                                    <label className="form-label">Last Name</label>
-                                    <input type="text" className="form-control" name="lastName" onChange={onChangeHandler} />
-                                </div>
+                            {/* <div className="col"> */}
+                            <div className="mb-3 d-flex justify-content-center">
+                                <TextField id="outlined-basic" type="text" name="lastName" label="Last Name" variant="outlined" onChange={onChangeHandler} />
+                                {/* <label className="form-label">Last Name</label>
+                                    <input type="text" className="form-control" name="lastName" onChange={onChangeHandler} /> */}
                             </div>
+                            {/* </div> */}
                         </div>
 
                         <div className="row">
-                            <div className="col">
-                                <div className="mb-3">
-                                    <label className="form-label">Email address</label>
-                                    <input type="email" className="form-control" name='email' onChange={onChangeHandler} />
-                                </div>
+                            {/* <div className="col"> */}
+                            <div className="mb-3 d-flex justify-content-center">
+                                <TextField id="outlined-basic" type="email" name="email" label="Email" variant="outlined" onChange={onChangeHandler} />
+                                {/* <label className="form-label">Email address</label>
+                                    <input type="email" className="form-control" name='email' onChange={onChangeHandler} /> */}
                             </div>
+                            {/* </div> */}
 
-                            <div className="col">
-                                <div className="mb-3">
-                                    <label className="form-label">Password</label>
-                                    <input type="password" className="form-control" name='password' onChange={onChangeHandler} />
-                                </div>
+                            {/* <div className="col"> */}
+                            <div className="mb-3 d-flex justify-content-center">
+                                <TextField id="outlined-basic" type="password" name="password" label="Password" variant="outlined" onChange={onChangeHandler} />
+                                {/* <label className="form-label">Password</label>
+                                    <input type="password" className="form-control" name='password' onChange={onChangeHandler} /> */}
                             </div>
+                            {/* </div> */}
                         </div>
-                        <button className="btn btn-primary" onClick={onSubmitHandler}>Submit</button>
+                        <div className="d-flex justify-content-center">
+                            <button className="btn btn-primary" onClick={onSubmitHandler}>Submit</button>
+                        </div>
                         <br />
                     </form>
                 </div>
--- frontend/src/scenes/Register/PumpRegister.jsx
+++ frontend/src/scenes/Register/PumpRegister.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate, useParams } from "react-router-dom";
 import axios from 'axios';
 import mainservice from '../../service/mainservice';
+import TextField from '@mui/material/TextField';
 //forhandler
 
 function PumpRegister() {
@@ -41,44 +42,49 @@ function PumpRegister() {
     };
     return (
         <>
-            <div className="p-4">
-                <div className="shadow p-4 mt-5 bg-body-tertiary rounded">
+            <div className="p-4 mt-5 d-flex justify-content-center">
+                <div className="shadow p-4 mt-3 bg-body-tertiary rounded">
                     <form>
-                        <h1 className="display-5 mb-4">Pump Register</h1>
+                        <h1 className="display-5 mb-4 d-flex justify-content-center">Pump Register</h1>
                         <div className="row">
-                            <div className="col">
-                                <div className="mb-3">
-                                    <label className="form-label">Pump Name</label>
-                                    <input type="text" className="form-control" name="PumpName" onChange={onChangeHandler} />
-                                </div>
+                            {/* <div className="col"> */}
+                            <div className="mb-3 d-flex justify-content-center">
+                                <TextField id="outlined-basic" type="text" name="PumpName" label="Pump Name" variant="outlined" onChange={onChangeHandler} />
+                                {/* <label className="form-label">Pump Name</label>
+                                    <input type="text" className="form-control" name="PumpName" onChange={onChangeHandler} /> */}
                             </div>
+                            {/* </div> */}
 
-                            <div className="col">
-                                <div className="mb-3">
-                                    <label className="form-label">Mobile</label>
-                                    <input type="text" className="form-control" name="PhoneNumber" onChange={onChangeHandler} />
-                                </div>
+                            {/* <div className="col"> */}
+                            <div className="mb-3 d-flex justify-content-center">
+                                <TextField id="outlined-basic" type="text" name="PhoneNumber" label="Mobile" variant="outlined" onChange={onChangeHandler} />
+                                {/* <label className="form-label">Mobile</label>
+                                    <input type="text" className="form-control" name="PhoneNumber" onChange={onChangeHandler} /> */}
                             </div>
+                            {/* </div> */}
                         </div>
 
                         <div className="row">
-                            <div className="col">
-                                <div className="mb-3">
-                                    <label className="form-label">Address</label>
-                                    <input type="text" className="form-control" name='Address' onChange={onChangeHandler} />
-                                </div>
+                            {/* <div className="col"> */}
+                            <div className="mb-3 d-flex justify-content-center">
+                                <TextField id="outlined-basic" type="text" name="Address" label="Address" variant="outlined" onChange={onChangeHandler} />
+                                {/* <label className="form-label">Address</label>
+                                    <input type="text" className="form-control" name='Address' onChange={onChangeHandler} /> */}
                             </div>
+                            {/* </div> */}
 
-                            <div className="col">
-                                <div className="mb-3">
-                                    <label className="form-label">Email</label>
-                                    <input type="email" className="form-control" name='email' onChange={onChangeHandler} />
-                                </div>
+                            {/* <div className="col"> */}
+                            <div className="mb-3 d-flex justify-content-center">
+                                <TextField id="outlined-basic" type="email" name="email" label="Email" variant="outlined" onChange={onChangeHandler} />
+                                {/* <label className="form-label">Email</label>
+                                    <input type="email" className="form-control" name='email' onChange={onChangeHandler} /> */}
                             </div>
+                            {/* </div> */}
                         </div>
-
-                        <button className="btn btn-primary" onClick={onSubmitHandler}>Submit</button>
-                        <br />
+                        <div className="d-flex justify-content-center">
+                            <button className="btn btn-primary" onClick={onSubmitHandler}>Submit</button>
+                            <br />
+                        </div>
                     </form>
                 </div>
             </div>
