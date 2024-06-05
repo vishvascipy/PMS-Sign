e93a45b3309c56bf069d1dbd09a174fb7a4255e5
vishvascipy <vishva.scipy@gmail.com>
2024-06-05 16:56:01 +0530

    about&features completed

--- frontend/src/scenes/components/About/AboutUs.jsx
+++ frontend/src/scenes/components/About/AboutUs.jsx
@@ -13,32 +13,35 @@ function AboutUs() {
         setFulltext(!fulltext)
     }
     return (
-        <div className='featuremain'>
-            <div className='background'>
-                <div className='img'><img src={About} /></div>
-                <div className='imgfeature'>
-                    <img src={Image} />
-                    <div className='text'>
-                        <h2>BEST FUEL</h2>
-                        <h2>ERP SOLUTION</h2>
-                        <p>
-                            {fulltext ? (
-                                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-                            ) : (
-                                "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-                            )}
-                        </p>
-                        <div className='button' onClick={toggletext}>
-                            <button>{fulltext ? 'Read Less' : 'Read More'}</button>
+        <div className='aboutmain'>
+            <div className='backgroundabout'>
+                <div className='backabout'>
+                    <div className='img'><img src={About} /></div>
+                    <div className='imgabout'>
+                        <img src={Image} />
+                        <div className='textabout'>
+                            <h2>BEST FUEL</h2>
+                            <h2>ERP SOLUTION</h2>
+                            <p>
+                                {fulltext ? (
+                                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                                ) : (
+                                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                                )}
+                            </p>
+                            <div className='button' onClick={toggletext}>
+                                <button>{fulltext ? 'Read Less' : 'Read More'}</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className='logo'>
-                    <img src={Logo} />
-                </div>
-                
-                <div className='bottomimg'>
-                    <img src={Bottom} />
+
+                    <div className='bottompic'>
+                        <img src={Bottom} />
+                    </div>
+
+                    <div className='logoabout'>
+                        <img src={Logo} />
+                    </div>
                 </div>
             </div>
         </div>
--- frontend/src/scenes/components/Features/Features.jsx
+++ frontend/src/scenes/components/Features/Features.jsx
@@ -14,35 +14,39 @@ function Features() {
     return (
         <div className='featuremain'>
             <div className='background'>
-                <div className='img'><img src={Feature} /></div>
-                <div className='imgfeature'>
-                    <img src={Image} />
-                    <div className='text'>
-                        <h2>BEST FUEL</h2>
-                        <h2>ERP SOLUTION</h2>
-                        <p>
-                            {fulltext ? (
-                                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-                            ) : (
-                                "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-                            )}
-                        </p>
-                        <div className='button' onClick={toggletext}>
-                            <button>{fulltext ? 'Read Less' : 'Read More'}</button>
+                <div className='back'>
+                    <div className='img'><img src={Feature} /></div>
+                    <div className='imgfeature'>
+                        <img src={Image} />
+                        <div className='text'>
+                            <h2>BEST FUEL</h2>
+                            <h2>ERP SOLUTION</h2>
+                            <p>
+                                {fulltext ? (
+                                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                                ) : (
+                                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                                )}
+                            </p>
+                            <div className='button' onClick={toggletext}>
+                                <button>{fulltext ? 'Read Less' : 'Read More'}</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className='side'>
-                    <p><a href='/Support'>24/7 Support</a></p>
-                    <p><a href='/CustomizeYourOwn'>Customize Your Own</a></p>
-                    <p><a href='/CloudBased'>Cloud Based</a></p>
-                </div>
+                    <div className='side'>
+                        <p><a href='/Support'>24/7 Support</a></p>
+                        <p><a href='/CustomizeYourOwn'>Customize Your Own</a></p>
+                        <p><a href='/CloudBased'>Cloud Based</a></p>
+                    </div>
 
-                <div className='bottomimg'>
-                    <img src={Bottom} />
-                </div>
-                <div className='logo'>
-                    <img src={Indhanlogo}/>
+
+                    <div className='bottomimg'>
+                        <img src={Bottom} />
+                    </div>
+
+                    <div className='logofeature'>
+                        <img src={Indhanlogo} />
+                    </div>
                 </div>
 
             </div>
