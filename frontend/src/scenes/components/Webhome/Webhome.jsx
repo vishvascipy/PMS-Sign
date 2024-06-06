0ca418073d246dda0c60044f54be983d127aed91
vishvascipy <vishva.scipy@gmail.com>
2024-06-06 17:03:28 +0530

    stable navbar

--- frontend/src/scenes/components/Webhome/Webhome.jsx
+++ frontend/src/scenes/components/Webhome/Webhome.jsx
@@ -1,20 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Webhome.css"
 import Xbig from "../assests/IndhanLogoScribble.png"
-import Logo from "../assests/Indhan X text 2.png"
+import Logo from "../assests/IndhanXtext2.png"
 
 
 function Webhome() {
+  const [text, setText] = useState(false)
+
+  const toggletext = () => {
+    setText(!text)
+  }
   return (
     <>
       <div className='main'>
         <h1 className='best'>BEST FUEL</h1>
+        <div className='restxt'>
+          <div className='readmore'>
+            <p>
+              {text ? (
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+              ) : (
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+              )}
+            </p>
+            <div className='button' onClick={toggletext}>
+              <button>{text ? 'Read Less' : 'Read More'}</button>
+            </div>
+          </div>
+        </div>
         <h1 className='erp'>ERP SOLUTION</h1>
+
         <div className='xbig'><img src={Xbig} /></div>
-        {/* <p className='text'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
-        <div className='para'>
-          <button>Read More</button>
-        </div> */}
+
         <div className='indhan'>
           <img src={Logo} />
         </div>
